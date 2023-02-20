9ff867c23cef5eac3bf59f89ecfa73bbb5f2910f
1132988 <119757490+1132988@users.noreply.github.com>
2023-02-20 13:39:50 +0300

    Добавьте файлы проекта.

---
+++ ЛР2 (Целые числа)/ЛР2 (Целые числа).cpp
@@ -0,0 +1,11 @@
+﻿# include <iostream>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+	int ab;
+	cout << "Введите двузначное число: ";
+	cin >> ab;
+	cout << "Число при перестановке цифр: " << ab / 10 + ab % 10 * 10 << endl;
+	return 0;
+}
